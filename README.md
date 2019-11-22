516d598059505d8d8f8603407df57e44ee2c7dd9
박지훈 <jjjhhhvvv@aipers.com>
2019-11-22 19:27:13 +0900

    [settings] sass 전처리기 설치 및 설정(css)  - 의존성 설치  - /dist/css/style.css 파일로 번들링 설정

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # es6-project-template
-# npm run build 명령을 통해 babel로 트래스파이링 진행 
+
+
+# webpack : 번들링 도구 
+# 설치하기 
+# $ npm install --save-dev webpack webpack-cli
+# 번들링 전에 파일 트랜스파일링 진행이 필요하므로 로더 설치
+# babel-loader 설치
+# $ npm install --save-dev babel-loader
+
+
+# npm run build 명령을 통해 babel로 프랜스파일링 진행 
 # src/js/main.js ==> dist/js/main.js 변환됨 
 # 변환된 파일 터미널 실행 : node /dist/js/main
 
@@ -10,6 +20,10 @@
 # 폴리필 이란?
 # 브라우저 마다 지원하는 스펙이 다르고 오래된 브라우에 대해서도 코드가 작동되어야 하므로
 # 지원 되지 않는 기능을 맞춰 준다는 개념
+# 폴리필 의존성 추가 
+# npm install babel-polyfill  
+
+
 # babel이 트랜스파일링 해주는데 "폴리필"이 필요한 이유 ?
 # new Promise(), Object.assign(), Array.from() 등과 같이 
 # ES5 이하로 대체할 수 없는 기능은 바벨에서 트랜스 파일링이 되지 않는다.
@@ -19,7 +33,14 @@
 # 폴리필은 개발환경 뿐만 아니라 실제 환경에서도 필요하므로 --save-dev 옵션을 제거 한다.
 # --dev-save 는 product 에는 포함되지 않기때문에.
 # webpack 엔트리에 "폴리필" 추가
-# 개발서버 설치 : npm install --save-dev webpack-dev-server 
+# 개발서버 설치 
+# npm install --save-dev webpack-dev-server 
 # 'start' 스크립트 추가 : webpack-dev-server --mode development --open 
 
-
+# Webpack 을 통해 Sass 컴파일
+# 의존성 설치 
+# npm install node-sass style-loader css-loader sass-loader --save-dev
+ 
+# 2가지 방법 
+# 1> bundle.js 에 css 코드 추가 
+# 2> 별도의 style.css 파일 생성 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
     // entry files
-    entry: ['@babel/polyfill', './src/js/main.js'],
+    entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
 
     // 컴파일 + 번들링 된 js 파일이 저장될 경로와 이름 지정
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
+    plugins: [
+        // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+        new MiniCssExtractPlugin({filename: 'css/style.css'})
+    ],
+
     module: {
         rules: [
             {
@@ -24,6 +31,17 @@ module.exports = {
                         plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 }
+            },
+            // sass settings
+            {
+                test: /\.scss$/,
+                use: [
+                    // "style-loader", // creates style nodes from JS strings
+                    MiniCssExtractPlugin.loader, // bundle css file
+                    "css-loader",   // translates CSS into CommonJS
+                    "sass-loader"   // compiles Sass to CSS, using Node Sass by default
+                ],
+                exclude: /node_modules/
             }
         ]
     },
